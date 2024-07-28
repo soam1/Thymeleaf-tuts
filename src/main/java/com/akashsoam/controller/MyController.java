538afe691009f4c7933dba9786b91b3e9ebdad22
soam1 <soamakash9@gmail.com>
2024-07-29 00:08:09 +0530

    iteration in thymeLeaf, how to loop

--- src/main/java/com/akashsoam/controller/MyController.java
+++ src/main/java/com/akashsoam/controller/MyController.java
@@ -3,6 +3,7 @@ package com.akashsoam.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -19,5 +20,15 @@ public class MyController {
 		model.addAttribute("name", "Akash Soam");
 		model.addAttribute("currentDate", new java.util.Date());
 		return "about";
+	}
+	
+	//handling iteration in thymeleaf
+	@GetMapping("/example-loop")
+	public String exampleLoop(Model model) {
+		System.out.println("inside example-loop handler");
+
+		java.util.List<String> names = List.of("Akash", "Soam", "Sohan", "Rohan");
+		model.addAttribute("names", names);
+		return "iterate";
 	}
 }
